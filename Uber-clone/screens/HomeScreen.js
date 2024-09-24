5c2d1822371fbcb81a4b3ae7e85090c959690667
lastoyster <kadampratiksha56@gmail.com>
2024-09-24 10:22:23 +0530

    first commit

---
+++ Uber-clone/screens/HomeScreen.js
@@ -0,0 +1,62 @@
+import { Text, View, StyleSheet, SafeAreaView, Image } from 'react-native';
+import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+import NavFavorites from '../components/NavFavorites';
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+          source={{
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/220px-Uber_logo_2018.svg.png',
+          }}
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false} // remove 'powered by Google'
+          minLength={2} // minimum length of text to search
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={600}
+        />
+
+        <NavOptions />
+        <NavFavorites />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
